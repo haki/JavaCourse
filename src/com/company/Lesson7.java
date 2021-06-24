8f9c3b9a0ea3d64e9b952908908545070cf5676f
Hakan Meral <hakanmeral99@gmail.com>
2021-06-24 09:26:33 +0300

    Lesson 7

--- src/com/company/Lesson7.java
+++ src/com/company/Lesson7.java
@@ -5,6 +5,79 @@ import java.util.Scanner;
 
 public class Lesson7 {
     public static void main(String[] args) {
+        ex1();
+        ex2();
+        ex3();
+        ex4();
+        ex5();
+        ex6();
+        ex7();
+        ex8();
+        ex9();
+        ex10();
+        ex11();
+    }
+
+    private static void ex11() {
+        final Scanner scanner = new Scanner(System.in);
+        final double a = Double.parseDouble(scanner.nextLine());
+        final int n = Integer.parseInt(scanner.nextLine());
+        System.out.println(pow(a, n));
+    }
+
+    public static double pow(double a, long n) {
+        if (n <= 0) {
+            return 1;
+        }
+        if (n % 2 == 0) {
+            return pow(a * a, n / 2);
+        } else {
+            return pow(a, n - 1) * a;
+        }
+    }
+
+    private static void ex10() {
+        Scanner scanner = new Scanner(System.in);
+        long sum = sum(scanner.nextInt());
+        System.out.println(sum);
+    }
+
+    public static long sum(int s) {
+        if (s <= 1) {
+            return s;
+        }
+        return s + sum(s - 1);
+    }
+
+    private static void ex9() {
+        final Scanner scanner = new Scanner(System.in);
+        printReverseCharByChar(scanner.nextLine());
+    }
+
+    public static void printReverseCharByChar(String s) {
+        if (s.length() > 0) {
+            int last = s.length() - 1;
+            System.out.print(s.charAt(last));
+            printReverseCharByChar(s.substring(0, last));
+        }
+    }
+
+    private static void ex8() {
+        Scanner scanner = new Scanner(System.in);
+        long[] array = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToLong(Long::parseLong)
+                .toArray();
+        int index = scanner.nextInt();
+        long value = scanner.nextLong();
+        addValueByIndex(array, index, value);
+        Arrays.stream(array).forEach(e -> System.out.print(e + " "));
+    }
+
+    public static void addValueByIndex(long[] array, int index, long value) {
+        array[index] += value;
+    }
+
+    private static void ex7() {
         final Scanner scanner = new Scanner(System.in);
         String[] values = scanner.nextLine().split("\\s+");
         int[] numbers = Arrays.stream(values)
@@ -14,7 +87,92 @@ public class Lesson7 {
         Arrays.stream(numbers).forEach(e -> System.out.print(e + " "));
     }
 
-    private static void sort(int[] numbers) {
+    public static void sort(int[] numbers) {
         Arrays.sort(numbers);
+    }
+
+    private static void ex6() {
+        final Scanner scanner = new Scanner(System.in);
+        final String type = scanner.nextLine();
+        switch (type) {
+            case "long":
+                final long longVal = Long.parseLong(scanner.nextLine());
+                final long longResult = getMaxMinusCurrent(longVal);
+                System.out.println(longResult);
+                break;
+            case "int":
+                final int intVal = Integer.parseInt(scanner.nextLine());
+                final int intResult = getMaxMinusCurrent(intVal);
+                System.out.println(intResult);
+                break;
+            case "short":
+                final short shortVal = Short.parseShort(scanner.nextLine());
+                final short shortResult = getMaxMinusCurrent(shortVal);
+                System.out.println(shortResult);
+                break;
+            default:
+                System.out.println("Unknown type found");
+                break;
+        }
+    }
+
+    public static long getMaxMinusCurrent(long val) {
+        return Long.MAX_VALUE - val;
+    }
+
+    public static int getMaxMinusCurrent(int val) {
+        return Integer.MAX_VALUE - val;
+    }
+
+    public static short getMaxMinusCurrent(short val) {
+        return (short) (Short.MAX_VALUE - val);
+    }
+
+    private static void ex5() {
+        System.out.println("We are robots!");
+    }
+
+    private static void ex4() {
+        System.out.println("Hello, Java!");
+    }
+
+    private static void ex3() {
+        Scanner scanner = new Scanner(System.in);
+        long n = Integer.parseInt(scanner.nextLine().trim());
+        System.out.println(factorial(n));
+    }
+
+    private static void ex2() {
+        Scanner scanner = new Scanner(System.in);
+        char letter = scanner.nextLine().charAt(0);
+        System.out.println(isVowel(letter) ? "YES" : "NO");
+    }
+
+    private static void ex1() {
+        final Scanner scanner = new Scanner(System.in);
+        final double d = scanner.nextDouble();
+        System.out.println(extractInt(d));
+    }
+
+    public static int extractInt(double d) {
+        return (int) d;
+    }
+
+    public static boolean isVowel(char ch) {
+        char[] vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        for (char v: vowels) {
+            if (v == ch) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static long factorial(long n) {
+        long res = 1;
+        for (int i = 2; i <= n; i++) {
+            res *= i;
+        }
+        return res;
     }
 }
